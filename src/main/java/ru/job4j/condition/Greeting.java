91dfd8499a2db6bda21ab6b69e044d0af68da675
Voltron7 <aquastar.minsk@gmail.com>
2022-03-22 10:26:54 +0300

    9.1. String

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+        public static void main(String[] args) {
+        String idea = "I like Java!" + "But I am a newbie.";
+        int year = 2022;
+        String a = idea + year;
+        System.out.println(a);
+        }
+}
