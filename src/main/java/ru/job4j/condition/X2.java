eb856daa6e9c410436fd9c8d279a959a9e64cfe9
Voltron7 <aquastar.minsk@gmail.com>
2022-03-23 18:15:28 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -6,18 +6,6 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
-    public static int calc1(int e, int f, int g, int h) {
-        return e * h * h + f * h + g;
-    }
-
-    public static int calc2(int i, int j, int k, int l) {
-        return i * l * l + j * l + k;
-    }
-
-    public static int calc3(int m, int n, int o, int p) {
-        return m * p * p + n * p + o;
-    }
-
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
@@ -29,19 +17,19 @@ public class X2 {
         int f = 1;
         int g = 1;
         int h = 1;
-        int rsl1 = X2.calc1(e, f, g, h);
+        int rsl1 = X2.calc(e, f, g, h);
         System.out.println(rsl1);
         int i = 1;
         int j = 1;
         int k = 0;
         int l = 1;
-        int rsl2 = X2.calc2(i, j, k, l);
+        int rsl2 = X2.calc(i, j, k, l);
         System.out.println(rsl2);
         int m = 1;
         int n = 1;
         int o = 1;
         int p = 0;
-        int rsl3 = X2.calc2(m, n, o, p);
+        int rsl3 = X2.calc(m, n, o, p);
         System.out.println(rsl3);
     }
 }
--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class X2Test {
 
     @Test
-    public void calc() {
+    public void whenA1B1C1X1Then3() {
             int a = 1;
             int b = 1;
             int c = 1;
@@ -17,35 +17,35 @@ public class X2Test {
         }
 
     @Test
-    public void calc1() {
+    public void whenE0F1G1H1Then2() {
             int e = 0;
             int f = 1;
             int g = 1;
             int h = 1;
             int expected = 2;
-            int rsl1 = X2.calc1(e, f, g, h);
+            int rsl1 = X2.calc(e, f, g, h);
             Assert.assertEquals(expected, rsl1);
     }
 
     @Test
-    public void calc2() {
+    public void whenI1J1K0L1Then2() {
             int i = 1;
             int j = 1;
             int k = 0;
             int l = 1;
             int expected = 2;
-            int rsl2 = X2.calc2(i, j, k, l);
+            int rsl2 = X2.calc(i, j, k, l);
             Assert.assertEquals(expected, rsl2);
     }
 
     @Test
-    public void calc3() {
+    public void whenM1N1O0P1Then2() {
             int m = 1;
             int n = 1;
             int o = 0;
             int p = 1;
             int expected = 2;
-            int rsl3 = X2.calc3(m, n, o, p);
+            int rsl3 = X2.calc(m, n, o, p);
             Assert.assertEquals(expected, rsl3);
     }
 }
