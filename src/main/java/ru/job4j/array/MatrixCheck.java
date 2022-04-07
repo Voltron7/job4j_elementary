288e3758b52c3970172dd0f871338fd342c06e41
Voltron7 <aquastar.minsk@gmail.com>
2022-04-07 11:00:58 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #285168]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,7 +4,7 @@ public class MatrixCheck {
 
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int index = 0; index <= board.length - 1; index++) {
+        for (int index = 0; index < board.length; index++) {
             if (board[row][index] != 'X') {
                 result = false;
                 break;
@@ -15,7 +15,7 @@ public class MatrixCheck {
 
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int index = 0; index <= board.length - 1; index++) {
+        for (int index = 0; index < board.length; index++) {
             if (board[index][column] != 'X') {
                 result = false;
                 break;
@@ -26,7 +26,7 @@ public class MatrixCheck {
 
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
-        for (int index = 0; index <= board.length - 1; index++) {
+        for (int index = 0; index < board.length; index++) {
             rsl[index] = board[index][index];
         }
         return rsl;
@@ -34,7 +34,7 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        for (int index = 0; index <= board.length - 1; index++) {
+        for (int index = 0; index < board.length; index++) {
             if (board[index][index] == 'X' && monoHorizontal(board, index) || monoVertical(board, index)) {
                     result = true;
                     break;
